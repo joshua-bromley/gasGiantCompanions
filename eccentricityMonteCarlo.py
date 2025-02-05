9c4383c8b6e7a0520a927e04ac89ff12c1534132
Joshua Bromley <joshua@mattbromley.com>
2025-02-05 16:13:04 -0500

    More analysis in eccentricity distributions

--- eccentricityMonteCarlo.py
+++ eccentricityMonteCarlo.py
@@ -5,12 +5,14 @@ from scipy import special as sp
 from scipy import stats as stats
 import corner
 from tqdm import tqdm
+import seaborn as sb
 
 def eccenRedraw(eccens, errors):
     newEccens = np.zeros_like(eccens)
     for i in range(len(eccens)):
         if not (np.isnan(eccens[i]) or np.isnan(errors[i])):
-            newEccens[i] = stats.norm.rvs(loc = eccens[i], scale = errors[i])
+            while newEccens[i] <= 0:
+                newEccens[i] = stats.norm.rvs(loc = eccens[i], scale = errors[i])
         elif np.isnan(errors[i]):
             while newEccens[i] <= 0:
                 newEccens[i] = stats.norm.rvs(loc = eccens[i], scale = 0.1)
@@ -21,6 +23,7 @@ planets = pd.read_csv("./data/gasGiantData.csv")
 gasGiants = planets.loc[(planets["pl_type"] == "WJ") | (planets["pl_type"] == "CJ") & (planets["pl_orbeccen"] > 0)]
 seCompanions = planets.loc[(planets["companion_type"] % 2 == 0) & (planets["pl_orbeccen"] > 0)]
 hjCompanions = planets.loc[(planets["companion_type"] % 7 == 0) & (planets["pl_orbeccen"] > 0)]
+wcjCompanions = planets.loc[((planets["companion_type"] % 11 == 0) | (planets["companion_type"] % 13 == 0)) & (planets["pl_orbeccen"] > 0)]
 
 gasGiantEccen = np.array(gasGiants["pl_orbeccen"])
 gasGiantEccenErr = (np.array(gasGiants["pl_orbeccenerr1"]) - np.array(gasGiants["pl_orbeccenerr2"]))/2
@@ -31,23 +34,67 @@ seCompanionEccenErr = (np.array(seCompanions["pl_orbeccenerr1"]) - np.array(seCo
 hjCompanionEccen = np.array(hjCompanions["pl_orbeccen"])
 hjCompanionEccenErr = (np.array(hjCompanions["pl_orbeccenerr1"]) - np.array(hjCompanions["pl_orbeccenerr2"]))/2
 
-results = np.zeros((5000,6))
+wcjCompanionEccen = np.array(wcjCompanions["pl_orbeccen"])
+wcjCompanionEccenErr = (np.array(wcjCompanions["pl_orbeccenerr1"]) - np.array(wcjCompanions["pl_orbeccenerr2"]))/2
+
+betaResults = np.zeros((2000,8))
+rayleighResults = np.zeros((2000,8))
 
 
-for i in tqdm(range(len(results))):
+for i in tqdm(range(len(betaResults))):
     gasGiantEccenRedraw = eccenRedraw(gasGiantEccen, gasGiantEccenErr)
     seCompanionEccenRedraw = eccenRedraw(seCompanionEccen, seCompanionEccenErr)
     hjCompanionEccenRedraw = eccenRedraw(hjCompanionEccen, hjCompanionEccenErr)
+    wcjCompanionEccenRedraw = eccenRedraw(wcjCompanionEccen, wcjCompanionEccenErr)
     ggFitResults = stats.fit(stats.beta, gasGiantEccenRedraw, bounds = ((0,100),(0,100)))
-    results[i,0] = ggFitResults.params[0]
-    results[i,1] = ggFitResults.params[1]
+    betaResults[i,0] = ggFitResults.params[0]
+    betaResults[i,1] = ggFitResults.params[1]
+    rayleighResults[i,0],rayleighResults[i,1] = stats.rayleigh.fit(gasGiantEccenRedraw)
     seFitResults = stats.fit(stats.beta, seCompanionEccenRedraw, bounds = ((0,100),(0,100)))
-    results[i,2] = seFitResults.params[0]
-    results[i,3] = seFitResults.params[1]
+    betaResults[i,2] = seFitResults.params[0]
+    betaResults[i,3] = seFitResults.params[1]
+    rayleighResults[i,2],rayleighResults[i,3] = stats.rayleigh.fit(seCompanionEccenRedraw)
     hjFitResults = stats.fit(stats.beta, hjCompanionEccenRedraw, bounds = ((0,100),(0,100)))
-    results[i,4] = hjFitResults.params[0]
-    results[i,5] = hjFitResults.params[1]
+    betaResults[i,4] = hjFitResults.params[0]
+    betaResults[i,5] = hjFitResults.params[1]
+    rayleighResults[i,4], rayleighResults[i,5] = stats.rayleigh.fit(hjCompanionEccenRedraw)
+    wcjFitResults = stats.fit(stats.beta, wcjCompanionEccenRedraw, bounds = ((0,100),(0,100)))
+    betaResults[i,6] = wcjFitResults.params[0]
+    betaResults[i,7] = wcjFitResults.params[1]
+    rayleighResults[i,6],rayleighResults[i,7] = stats.rayleigh.fit(wcjCompanionEccenRedraw)
 
 
-fig = corner.corner(results, labels = ["GG a", "GG b", "SEc a", "SEc b", "HJc a", "HJc b"])
-fig.savefig("./plots/betaFitsCorner.png")
+fig, ax = plt.subplots(1,1)
+sb.kdeplot(x = betaResults[:,0], y = betaResults[:,1], ax = ax, fill = True)
+sb.kdeplot(x = betaResults[:,2], y = betaResults[:,3], ax = ax, fill = True)
+sb.kdeplot(x = betaResults[:,4], y = betaResults[:,5], ax = ax, fill = True)
+sb.kdeplot(x = betaResults[:,6], y = betaResults[:,7], ax = ax, fill = True)
+ax.set_xlabel("$\\alpha$")
+ax.set_ylabel("$\\beta$")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:blue", label = "All Gas Giants")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:orange", label = "SE Companions")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:green", label = "HJ Companions")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:red", label = "WCJ Companions")
+ax.set_xlim(0.5,3)
+ax.set_ylim(1,7)
+ax.legend(frameon = False)
+fig.savefig("./plots/betaFitsKDE.png")
+
+fig, ax = plt.subplots(1,1)
+sb.kdeplot(x = rayleighResults[:,0], y = rayleighResults[:,1], ax = ax, fill = True)
+sb.kdeplot(x = rayleighResults[:,2], y = rayleighResults[:,3], ax = ax, fill = True)
+sb.kdeplot(x = rayleighResults[:,4], y = rayleighResults[:,5], ax = ax, fill = True)
+sb.kdeplot(x = rayleighResults[:,6], y = rayleighResults[:,7], ax = ax, fill = True)
+ax.set_xlabel("loc")
+ax.set_ylabel("$\sigma$")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:blue", label = "All Gas Giants")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:orange", label = "SE Companions")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:green", label = "HJ Companions")
+ax.plot(-10,-10, ls = "", marker = ".", color = "tab:red", label = "WCJ Companions")
+ax.legend(frameon = False)
+ax.set_xlim(-0.15,0.1)
+ax.set_ylim(0.15,0.4)
+fig.savefig("./plots/rayleighFitsKDE.png")
+
+
+
--- metallicityDependence.py
+++ metallicityDependence.py
@@ -5,17 +5,36 @@ from scipy import stats as stats
 
 clsStars = pd.read_csv("./data/clsStars.csv")
 clsPlanets = pd.read_csv("./data/clsPlanets2.csv", skiprows = 101)
+clsPlanets = clsPlanets.loc[(clsPlanets["pl_bmasse"] > 0.5*317) & (clsPlanets["pl_orbsmax"] > 1)]
 
 planets = pd.read_csv("./data/gasGiantData.csv")
 
 superEarths = planets.loc[planets["pl_type"] == "SE"]
-seCompanions = planets.loc[planets["companion_type"] % 2 == 0]
+seCompanions = planets.loc[(planets["companion_type"] % 2 == 0) & (planets["pl_orbsmax"] > 1)]
 
-metallicityCutoffs = np.linspace(-0.2,0.2)
+outerCompanions = planets.loc[planets["companion_type"] > 1] 
+
+gasGiants = planets.loc[(planets["pl_type"] == "WJ") | (planets["pl_type"] == "CJ")]
+ggCompanions = planets.loc[(planets["companion_type"] % 11 == 0) | (planets["companion_type"] % 13 == 0)]
 
 
-metalRichEnhancement = np.zeros_like(metallicityCutoffs)
-metalPoorEnhancement = np.zeros_like(metallicityCutoffs)
+metallicityCutoffs = np.linspace(-0.2,0.3)
+
+
+SEmetalRichEnhancement = np.zeros_like(metallicityCutoffs)
+SEmetalPoorEnhancement = np.zeros_like(metallicityCutoffs)
+
+
+CompMetalRichEnhancement = np.zeros_like(metallicityCutoffs)
+CompMetalPoorEnhancement = np.zeros_like(metallicityCutoffs)
+
+GGmetalRichEnhancement = np.zeros_like(metallicityCutoffs)
+GGmetalPoorEnhancement = np.zeros_like(metallicityCutoffs)
+
+clsMetMedian = np.median(clsStars["[Fe/H]"].dropna())
+
+print(np.min(clsStars["[Fe/H]"]), np.max(clsStars["[Fe/H]"]))
+
 
 for i in range(len(metallicityCutoffs)):
     cutoff = metallicityCutoffs[i]
@@ -31,10 +50,25 @@ for i in range(len(metallicityCutoffs)):
     nMetalRichSECompanions = len(np.unique(seCompanions.loc[seCompanions["st_met"] > cutoff]["hostname"]))
     nMetalPoorSECompanions = len(np.unique(seCompanions.loc[seCompanions["st_met"] <= cutoff]["hostname"]))
 
+    nMetalRichCompanions = len(np.unique(outerCompanions.loc[outerCompanions["st_met"] > cutoff]["hostname"]))
+    nMetalPoorCompanions = len(np.unique(outerCompanions.loc[outerCompanions["st_met"] <= cutoff]["hostname"]))
+    nMetalRichInner = len(np.unique(planets.loc[planets["st_met"] > cutoff]["hostname"]))
+    nMetalPoorInner = len(np.unique(planets.loc[planets["st_met"] <= cutoff]["hostname"]))
+
+    nMetalRichGG = len(np.unique(gasGiants.loc[gasGiants["st_met"] > cutoff]["hostname"]))
+    nMetalPoorGG = len(np.unique(gasGiants.loc[gasGiants["st_met"] <= cutoff]["hostname"]))
+    nMetalRichGGCompanions = len(np.unique(ggCompanions.loc[ggCompanions["st_met"] > cutoff]["hostname"]))
+    nMetalPoorGGCompanions = len(np.unique(ggCompanions.loc[ggCompanions["st_met"] <= cutoff]["hostname"]))
+
     occurRateCLSMetalRich = stats.beta.median(nclsMetalRichPlanets + 1, nclsMetalRichStars - nclsMetalRichPlanets + 1)
     occurRateCLSMetalPoor = stats.beta.median(nclsMetalPoorPlanets + 1, nclsMetalPoorStars - nclsMetalPoorPlanets + 1)
     occurRateSEMetalRich = stats.beta.median(nMetalRichSECompanions, nMetalRichSE - nMetalRichSECompanions + 1)
     occurRateSEMetalPoor = stats.beta.median(nMetalPoorSECompanions, nMetalPoorSE - nMetalPoorSECompanions + 1)
+    occurRateCompMetalRich = stats.beta.median(nMetalRichCompanions + 1, nMetalRichInner - nMetalRichCompanions + 1)
+    occurRateCompMetalPoor = stats.beta.median(nMetalPoorCompanions + 1, nMetalPoorInner - nMetalPoorCompanions +1)
+    occurRateGGMetalRich = stats.beta.median(nMetalRichGGCompanions, nMetalRichGG - nMetalRichGGCompanions + 1)
+    occurRateGGMetalPoor = stats.beta.median(nMetalPoorGGCompanions, nMetalPoorGG - nMetalPoorGGCompanions + 1)
+    
 
     if occurRateSEMetalRich > occurRateCLSMetalRich:
         sigmaCLSMetalRich = stats.beta.interval(0.68,nclsMetalRichPlanets + 1, nclsMetalRichStars - nclsMetalRichPlanets + 1)[1] - occurRateCLSMetalRich
@@ -50,13 +84,36 @@ for i in range(len(metallicityCutoffs)):
         sigmaCLSMetalPoor = occurRateCLSMetalPoor -  stats.beta.interval(0.68,nclsMetalPoorPlanets + 1, nclsMetalPoorPlanets - nclsMetalPoorPlanets + 1)[0]
         sigmaSEMetalPoor= stats.beta.interval(0.68, nMetalPoorSECompanions, nMetalPoorSE - nMetalPoorSECompanions + 1)[1] - occurRateSEMetalPoor
 
-    metalRichEnhancement[i] = (occurRateSEMetalRich - occurRateCLSMetalRich)/np.sqrt(sigmaSEMetalRich*sigmaSEMetalRich + sigmaCLSMetalRich*sigmaCLSMetalRich)
-    metalPoorEnhancement[i] = (occurRateSEMetalPoor - occurRateCLSMetalPoor)/np.sqrt(sigmaSEMetalPoor*sigmaSEMetalPoor + sigmaCLSMetalPoor*sigmaCLSMetalPoor)
+    if occurRateCompMetalRich > occurRateCLSMetalRich:
+        sigmaCLSMetalRich = stats.beta.interval(0.68,nclsMetalRichPlanets + 1, nclsMetalRichStars - nclsMetalRichPlanets + 1)[1] - occurRateCLSMetalRich
+        sigmaCompMetalRich = occurRateCompMetalRich - stats.beta.interval(0.68, nMetalRichCompanions, nMetalRichInner - nMetalRichCompanions + 1)[0]
+    else:
+        sigmaCLSMetalRich = occurRateCLSMetalRich - stats.beta.interval(0.68,nclsMetalRichPlanets + 1, nclsMetalRichStars - nclsMetalRichPlanets + 1)[0]
+        sigmaCompMetalRich = stats.beta.interval(0.68, nMetalRichCompanions, nMetalRichInner - nMetalRichCompanions + 1)[1] - occurRateCompMetalRich
+    
+    if occurRateCompMetalPoor > occurRateCLSMetalPoor:
+        sigmaCLSMetalPoor = stats.beta.interval(0.68,nclsMetalPoorPlanets + 1, nclsMetalPoorPlanets - nclsMetalPoorPlanets + 1)[1] - occurRateCLSMetalPoor
+        sigmaCompMetalPoor= occurRateCompMetalPoor - stats.beta.interval(0.68, nMetalPoorCompanions, nMetalPoorInner - nMetalPoorCompanions + 1)[0]
+    else:
+        sigmaCLSMetalPoor = occurRateCLSMetalPoor -  stats.beta.interval(0.68,nclsMetalPoorPlanets + 1, nclsMetalPoorPlanets - nclsMetalPoorPlanets + 1)[0]
+        sigmaCompMetalPoor= stats.beta.interval(0.68, nMetalPoorCompanions, nMetalPoorInner - nMetalPoorCompanions + 1)[1] - occurRateCompMetalPoor
+
+
+        
+    SEmetalRichEnhancement[i] = (occurRateSEMetalRich - occurRateCLSMetalRich)/np.sqrt(sigmaSEMetalRich*sigmaSEMetalRich + sigmaCLSMetalRich*sigmaCLSMetalRich)
+    SEmetalPoorEnhancement[i] = (occurRateSEMetalPoor - occurRateCLSMetalPoor)/np.sqrt(sigmaSEMetalPoor*sigmaSEMetalPoor + sigmaCLSMetalPoor*sigmaCLSMetalPoor)
+
+    CompMetalRichEnhancement[i] = (occurRateCompMetalRich - occurRateCLSMetalRich)/np.sqrt(sigmaCompMetalRich*sigmaCompMetalRich + sigmaCLSMetalRich*sigmaCLSMetalRich)
+    CompMetalPoorEnhancement[i] = (occurRateCompMetalPoor - occurRateCLSMetalPoor)/np.sqrt(sigmaCompMetalPoor*sigmaCompMetalPoor + sigmaCLSMetalPoor*sigmaCLSMetalPoor)
+ 
 
 
 fig, ax = plt.subplots(1,1)
-ax.plot(metallicityCutoffs, metalRichEnhancement, ls = "", marker = "o", label = "[Fe/H] > x")
-ax.plot(metallicityCutoffs, metalPoorEnhancement, ls = "", marker = "o", label = "[Fe/H] <= x")
+ax.plot(metallicityCutoffs, SEmetalRichEnhancement, ls = "", marker = "o", label = "[Fe/H] > x, SE Inner")
+ax.plot(metallicityCutoffs, SEmetalPoorEnhancement, ls = "", marker = "o", label = "[Fe/H] <= x, SE Inner")
+#ax.plot(metallicityCutoffs, CompMetalRichEnhancement, ls = "", marker = "o", label = "[Fe/H] > x, Any Inner")
+#ax.plot(metallicityCutoffs, CompMetalPoorEnhancement, ls = "", marker = "o", label = "[Fe/H] <= x, Any Inner")
+ax.vlines(clsMetMedian, 0,2.5)
 ax.legend(frameon = False)
 ax.set_ylabel("Enhancement ($\sigma$)")
 ax.set_xlabel('Metallicity Cutoff ([Fe/H])')
