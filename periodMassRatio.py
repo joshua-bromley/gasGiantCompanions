84553a2b3b3013f949c526ed4ae1fbb0d23d0706
Joshua Bromley <joshua@mattbromley.com>
2025-03-03 15:53:18 -0500

    More analysis code concerning period mass ratios

--- periodMassRatio.py
+++ periodMassRatio.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot as plt
 planets = pd.read_csv("./data/gasGiantData.csv").drop_duplicates(subset = ["pl_name"])
 
 planetPairs = pd.DataFrame(columns=["hostname","planet_a_name", "planet_b_name", "pla_per", "plb_per","per_ratio", "pla_mass", "plb_mass", "mass_ratio", "pla_eccen", "plb_eccen","st_mass","st_met"])
+hjPairs = pd.DataFrame(columns=["hostname","planet_a_name", "planet_b_name", "pla_per", "plb_per","per_ratio", "pla_mass", "plb_mass", "mass_ratio", "pla_eccen", "plb_eccen","st_mass","st_met"])
+wcjPairs = pd.DataFrame(columns=["hostname","planet_a_name", "planet_b_name", "pla_per", "plb_per","per_ratio", "pla_mass", "plb_mass", "mass_ratio", "pla_eccen", "plb_eccen","st_mass","st_met"])
+
 
 hostnames = np.unique(planets["hostname"])
 
@@ -27,7 +30,11 @@ for i in range(len(hostnames)):
         massRatio = planetAMass/planetBMass
         perRatio = planetAPer/planetBPer
         planetPairs.loc[len(planetPairs)] = [hostname, planetAName, planetBName, planetAPer, planetBPer, perRatio, planetAMass, planetBMass, massRatio, planetAeccen, planetBeccen, stellarMass, stellarMet]
-
+        if planetB["pl_type"] == "HJ" and (planetA["pl_type"] == "WJ" or planetA["pl_type"] == 'CJ'):
+            hjPairs.loc[len(hjPairs)] = [hostname, planetAName, planetBName, planetAPer, planetBPer, perRatio, planetAMass, planetBMass, massRatio, planetAeccen, planetBeccen, stellarMass, stellarMet]
+        if (planetB["pl_type"] == "WJ" or planetB["pl_type"] == "CJ") and (planetA["pl_type"] == "WJ" or planetA["pl_type"] == "CJ"):
+            wcjPairs.loc[len(wcjPairs)] = [hostname, planetAName, planetBName, planetAPer, planetBPer, perRatio, planetAMass, planetBMass, massRatio, planetAeccen, planetBeccen, stellarMass, stellarMet]
+ 
 
 highMassPairs = planetPairs.loc[planetPairs["pla_mass"] > 0.5*317]
 
@@ -35,12 +42,28 @@ periodRatios = planetPairs["per_ratio"].values
 massRatios = planetPairs["mass_ratio"].values
 outerEccen = planetPairs["pla_eccen"].values
 
+hjPerRatios = hjPairs["per_ratio"].values
+hjMassRatios = hjPairs["mass_ratio"].values
+
+wcjPerRatios = wcjPairs["per_ratio"].values
+wcjMassRatios = wcjPairs["mass_ratio"].values
+
 fig, ax = plt.subplots(1,1)
-ax.plot(periodRatios, massRatios, ls = "", marker = "o")
+ax.plot(hjPairs["plb_mass"].values, hjPairs["pla_mass"].values, ls = "", marker = "o")
+ax.plot(wcjPairs["plb_mass"].values, wcjPairs["pla_mass"].values, ls = "", marker = "o")
+for i in range(len(hjPairs)):
+    ax.text(hjPairs["plb_mass"].values[i], hjPairs["pla_mass"].values[i], hjPairs.iloc[i]["hostname"])
 ax.set_xscale("log")
 ax.set_yscale("log")
+ax.set_xlabel("Inner Planet Period (d)")
+ax.set_ylabel('Outer Planet Period (d)')
+#fig.savefig("./plots/perMassRatio.png")
 
 
-plt.show()
+
+print(np.mean(hjMassRatios))
+print(np.mean(wcjMassRatios))
 
         
+
+plt.show()
