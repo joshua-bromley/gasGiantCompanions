6ad0052ce3ff2be663f88bd8a6ec22a134473202
Joshua Bromley <joshua@mattbromley.com>
2025-08-15 10:49:52 -0400

    More RV data for short period planets (only WASP planets left)

--- identifyCandidates.py
+++ identifyCandidates.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
 
-candidates = pd.read_csv("./data/sampleCandidates.csv", skiprows=100)
+seenCandidates = pd.read_csv("./data/sampleCandidates.csv", skiprows = 100)
+candidates = pd.read_csv("./data/sampleCandidatesSP.csv", skiprows=99)
 sample = pd.read_csv("./data/gasGiantDataComplete2.csv")
 
+seenSystems = np.unique(seenCandidates["hostname"])
 candidateSystems = np.unique(candidates["hostname"])
 sampleSystems = np.unique(sample["hostname"])
 
 remainingCandidates = []
+inSample = []
 
 for i in range(len(candidateSystems)):
-    if not candidateSystems[i] in sampleSystems:
+    if not candidateSystems[i] in sampleSystems and not candidateSystems[i] in seenSystems:
         remainingCandidates.append(candidateSystems[i])
+    else:
+        inSample.append(candidateSystems[i])
 
-np.savetxt("./sampleCandidates.txt", remainingCandidates, fmt = "%s")
+np.savetxt("./sampleCandidatesSP.txt", remainingCandidates, fmt = "%s")
