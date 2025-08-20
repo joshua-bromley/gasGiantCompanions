1cfeabb16e99e6a6b623f23990874e97455b4dd6
Joshua Bromley <joshua@mattbromley.com>
2025-08-20 13:03:46 -0400

    Compiled all the data from the exoplanet archive

--- completeRVDataNames.py
+++ completeRVDataNames.py
@@ -7,10 +7,12 @@ defaultSource = systemData.loc[pd.isna(systemData["data_ref"]) == True]
 clsSource = systemData.loc[systemData["data_ref"] == "Rosenthal+21"]
 bonomoSource = systemData.loc[(systemData["prop_ref"] == "Bonomo+17") | (systemData["data_ref"] == "Bonomo+17") | (systemData["data_ref"] == "Bonomo+23")]
 weissSource = systemData.loc[systemData["data_ref"] == "Weiss+24"]
-otherSource = systemData[(pd.isna(systemData["data_ref"]) == False) & (systemData["data_ref"] != "Rosenthal+21") & (systemData["prop_ref"] != "Bonomo+17") & (systemData["data_ref"] != "Bonomo+17") & (systemData["data_ref"] != "Bonomo+23") & (systemData["data_ref"] != "Weiss+24")]
+polanskiSource = systemData.loc[systemData["data_ref"] == "Polanski+24"]
+otherSource = systemData[(pd.isna(systemData["data_ref"]) == False) & (systemData["data_ref"] != "Rosenthal+21") & (systemData["prop_ref"] != "Bonomo+17") & (systemData["data_ref"] != "Bonomo+17") & (systemData["data_ref"] != "Bonomo+23") & (systemData["data_ref"] != "Weiss+24")  & (systemData["data_ref"] != "Polanski+24")]
 
 np.savetxt("./namelists/nameListDefaultSource.txt", defaultSource["hostname"].values, fmt="%s")
 np.savetxt("./namelists/namelistCLSSource.txt", clsSource["hostname"].values, fmt="%s")
 np.savetxt("./namelists/namelistBonomoSource.txt", bonomoSource["hostname"].values, fmt="%s")
 np.savetxt("./namelists/namelistWeissSource.txt", weissSource[["hostname"]].values, fmt = "%s")
-np.savetxt("./namelists/namelistOtherSource.txt", otherSource[["hostname", "data_ref"]].values, fmt="%s")
+np.savetxt("./namelists/namelistPolanskiSource.txt", polanskiSource[["hostname"]].values, fmt = "%s")
+np.savetxt("./namelists/namelistOtherSource.txt", otherSource[["hostname"]].values, fmt="%s")
